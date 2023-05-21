e14613e48298053a58c6f3a98340058224d0b67f
khanh.truong <abby79truong@gmail.com>
2023-05-21 06:07:11 +0200

    integrated the homepage & created the naviagtion btw the homepage & photographer page

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -6,24 +6,44 @@ function photographerFactory(data) {
   const picture = `assets/photographers/PhotographersID/${portrait}`;
 
   function getUserCardDOM() {
+    // créer une zone focusable contenant le h2 et l'image:
+    // => fonction getUserCardDOM creates an element <a>: contains article (image, h2 & the p(s))
+    const link = document.createElement("a");
+    link.href = "photographer.html";
+
     const article = document.createElement("article");
 
     const img = document.createElement("img");
-    img.setAttribute("src", picture);
+    img.src = picture;
+    img.alt = " " + name;
 
     const h2 = document.createElement("h2");
     h2.textContent = name;
-
-    const p = document.createElement("p");
-    p.innerHTML = `</span>${city}, ${country}</span><br /> 
-        <span>${tagline}</span><br />
-        <span>${price}€/jour</span>`;
+    const p1 = document.createElement("p1");
+    p1.innerHTML = `${city}, ${country}`;
+    const p2 = document.createElement("p2");
+    p2.innerHTML = `${tagline}`;
+    const p3 = document.createElement("p3");
+    p3.innerHTML = `${price}€/jour`;
 
     article.appendChild(img);
     article.appendChild(h2);
-    article.appendChild(p);
+    article.appendChild(p1);
+    article.appendChild(p2);
+    article.appendChild(p3);
 
-    return article;
+    link.appendChild(article);
+
+    return link;
+    // return article;
   }
   return { name, picture, getUserCardDOM };
 }
+
+// const p = document.createElement("p");
+// p.innerHTML = `<span>${city}, ${country}</span><br/>
+//     <span>${tagline}</span><br/>
+//     <span>${price}€/jour</span>`;
+
+// const imageUrl = document.getElementsByTagName("img").style.backgroundImage;
+// img.setAttribute("src", picture);
